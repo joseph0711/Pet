c62e34d3e644aa32625106a49c58802f35bcca73
Joseph Chiang <joseph900711@gmail.com>
2024-05-25 20:19:01 +0800

    Add maven property for paho.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 pluginManagement {
     repositories {
+        maven{
+            url = uri("https://repo.eclipse.org/content/repositories/paho-snapshots/")
+        }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,6 +17,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven{
+            url = uri("https://repo.eclipse.org/content/repositories/paho-snapshots/")
+        }
         google()
         mavenCentral()
     }
