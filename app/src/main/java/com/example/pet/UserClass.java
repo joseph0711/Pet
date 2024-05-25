159aaa68b911d549abd930197c6c3e9ee3a5cb5b
Joseph Chiang <joseph900711@gmail.com>
2024-05-26 00:31:45 +0800

    Add get/set methods.

--- app/src/main/java/com/example/pet/UserClass.java
+++ app/src/main/java/com/example/pet/UserClass.java
@@ -4,4 +4,11 @@ public class UserClass {
     public int id;
     public String name;
     public String createdDateTime;
+
+    public void setCreatedDateTime(String createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+    public String getCreatedDateTime() {
+        return createdDateTime;
+    }
 }
