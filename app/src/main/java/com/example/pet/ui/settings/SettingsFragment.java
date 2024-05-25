12b2a607a8c9febccdc28e728b0d15134fad8002
Joseph Chiang <joseph900711@gmail.com>
2024-05-25 20:18:39 +0800

    Add fragment for settings.

--- app/src/main/java/com/example/pet/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/pet/ui/settings/SettingsFragment.java
@@ -1,16 +1,41 @@
 package com.example.pet.ui.settings;
 
 import android.os.Bundle;
-import androidx.preference.PreferenceFragmentCompat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.pet.ui.changeUserInfo.ChangeUserInfoFragment;
 import com.example.pet.R;
+import com.example.pet.databinding.FragmentSettingsBinding;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends Fragment {
+    private FragmentSettingsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        Button btnModify = root.findViewById(R.id.settings_btnModify);
+        btnModify.setOnClickListener(view -> {
+            Fragment fragment = new ChangeUserInfoFragment();
+            FragmentTransaction fragmentTransaction = requireActivity().getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.container, fragment).commit();
+        });
+        return root;
+    }
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.root_preferences);
-        //setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
