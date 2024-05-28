83852b6f7914ac2dc467d367513b1c8641368f50
Joseph Chiang <joseph900711@gmail.com>
2024-05-28 22:24:49 +0800

    Add get and set method.

--- app/src/main/java/com/example/pet/UserClass.java
+++ app/src/main/java/com/example/pet/UserClass.java
@@ -11,4 +11,16 @@ public class UserClass {
     public String getCreatedDateTime() {
         return createdDateTime;
     }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 }
