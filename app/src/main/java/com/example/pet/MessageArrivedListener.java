2a89eb88c8d0a82b39c76c76cda90ba20ce15227
Joseph Chiang <joseph900711@gmail.com>
2024-05-30 02:46:33 +0800

    Delete debug tools.

--- app/src/main/java/com/example/pet/MessageArrivedListener.java
+++ app/src/main/java/com/example/pet/MessageArrivedListener.java
@@ -7,8 +7,6 @@ import com.example.pet.ui.feeding.FeedingFragment;
 public class MessageArrivedListener {
     public String result;
     public void messageArrived() {
-        Log.i("INFO", "result: " + result);
-        Log.i("INFO", "Executing messageArrived()");
         FeedingFragment feedingFragment = new FeedingFragment();
         feedingFragment.updateTextView(result);
     }
