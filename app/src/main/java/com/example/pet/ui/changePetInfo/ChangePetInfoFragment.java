1a7e3b483eea5c6179d525323fd3351902753323
Joseph Chiang <joseph900711@gmail.com>
2024-06-05 03:05:10 +0800

    Add pet avatar finding method by user id.

--- app/src/main/java/com/example/pet/ui/changePetInfo/ChangePetInfoFragment.java
+++ app/src/main/java/com/example/pet/ui/changePetInfo/ChangePetInfoFragment.java
@@ -1,22 +1,32 @@
-package com.example.pet.ui.changeUserInfo;
+package com.example.pet.ui.changePetInfo;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -24,17 +34,21 @@ import android.widget.Toast;
 
 import com.example.pet.ConnectionMysqlClass;
 import com.example.pet.R;
+import com.example.pet.SharedViewModel;
 import com.example.pet.ui.login.LoginActivity;
 
+import java.io.ByteArrayOutputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Calendar;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class ChangeUserInfoFragment extends Fragment {
+public class ChangePetInfoFragment extends Fragment {
+    private SharedViewModel sharedViewModel;
     ConnectionMysqlClass connectionMysqlClass;
     Connection con;
     String str;
@@ -42,29 +56,46 @@ public class ChangeUserInfoFragment extends Fragment {
     private RadioButton radioGenderButton;
     private Button btnConfirm, btnDatePicker;
     private TextView textDatePicked, textAge;
+    private ImageView petInfoAvatar;
     int day, month, year;
     String userInputDate;
     Period period;
+    ActivityResultLauncher<Intent> resultLauncher;
+    private ActivityResultLauncher<String> requestPermissionLauncher;
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_change_user_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_change_pet_info, container, false);
+
+        // Get the view model.
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
+        // Call the photo picker when user clicks on the avatar.
+        petInfoAvatar = view.findViewById(R.id.changePetInfo_imgAvatar);
+        petInfoAvatar.setOnClickListener(v -> {
+            Toast.makeText(requireContext(), "Pick Image", Toast.LENGTH_SHORT).show();
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                pickImage();
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        });
 
         // Get the value from the radio button in radio group.
-        RadioGroup radioGenderGroup = view.findViewById(R.id.changeUserInfo_radioGenderGroup);
+        RadioGroup radioGenderGroup = view.findViewById(R.id.changePetInfo_radioGenderGroup);
         radioGenderGroup.setOnCheckedChangeListener((radioGroup, i) -> {
-            if (i == R.id.changeUserInfo_radioBtnMale) {
-                radioGenderButton = view.findViewById(R.id.changeUserInfo_radioBtnMale);
+            if (i == R.id.changePetInfo_radioBtnMale) {
+                radioGenderButton = view.findViewById(R.id.changePetInfo_radioBtnMale);
             } else {
-                radioGenderButton = view.findViewById(R.id.changeUserInfo_radioBtnFemale);
+                radioGenderButton = view.findViewById(R.id.changePetInfo_radioBtnFemale);
             }
         });
 
         // Set the date picker for the Birth Date.
-        btnDatePicker = view.findViewById(R.id.changeUserInfo_btnDatePicker);
-        textDatePicked = view.findViewById(R.id.changeUserInfo_textDatePicked);
-        textAge = view.findViewById(R.id.changeUserInfo_textAge);
+        btnDatePicker = view.findViewById(R.id.changePetInfo_btnDatePicker);
+        textDatePicked = view.findViewById(R.id.changePetInfo_textDatePicked);
+        textAge = view.findViewById(R.id.changePetInfo_textAge);
         btnDatePicker.setOnClickListener(view1 -> {
             Calendar calendar = Calendar.getInstance();
             day = calendar.get(Calendar.DAY_OF_MONTH);
@@ -98,21 +129,57 @@ public class ChangeUserInfoFragment extends Fragment {
             datePickerDialog.show();
         });
 
-        petNameEditText = view.findViewById(R.id.changeUserInfo_inputName);
-        weightEditText = view.findViewById(R.id.changeUserInfo_inputWeight);
+        petNameEditText = view.findViewById(R.id.changePetInfo_inputName);
+        weightEditText = view.findViewById(R.id.changePetInfo_inputWeight);
 
-        // Call the petInfoRegister() method when the Confirm button is clicked.
-        btnConfirm = view.findViewById(R.id.changeUserInfo_btnConfirm);
+        // Make a connection to MySQL.
+        connectionMysqlClass = new ConnectionMysqlClass();
+        connectMysql();
+
+        // Call the petInfoUpdate() method when the Confirm button is clicked.
+        btnConfirm = view.findViewById(R.id.changePetInfo_btnConfirm);
         btnConfirm.setOnClickListener( view1 -> petInfoUpdate());
         return view;
     }
 
+    // Request permission to pick image from gallery.
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check for permission to read external storage.
+        requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                pickImage();
+            } else {
+                Toast.makeText(requireContext(), "Permission required to pick image", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Initialize the ActivityResultLauncher instance for handling the result of an activity that is started for a result.
+        resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Uri imageUri = result.getData().getData();
+                petInfoAvatar.setImageURI(imageUri);
+            }
+        });
+    }
+
+    // Pick image from gallery.
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        resultLauncher.launch(intent);
+    }
+
     private void petInfoUpdate() {
+        int id = Objects.requireNonNull(sharedViewModel.getUserClass().getValue()).id;
         String petName, birthDate, gender;
-        int age, weight;
+        byte[] imageBytes;
+        int age;
+        float weight;
         petName = petNameEditText.getText().toString();
         birthDate = userInputDate;
-        weight = Integer.parseInt(weightEditText.getText().toString());
+        weight = Float.parseFloat(weightEditText.getText().toString());
         gender = radioGenderButton.getText().toString();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             age = period.getYears();
@@ -120,17 +187,25 @@ public class ChangeUserInfoFragment extends Fragment {
             age = 0;
         }
 
-        String sql = "INSERT INTO pet (Name, Weight, Age, BirthDate, Gender) VALUES (?, ?, ?, ?, ?);";
+        // Convert the image to a byte array.
+        Bitmap bitmap = ((BitmapDrawable) petInfoAvatar.getDrawable()).getBitmap();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        imageBytes = outputStream.toByteArray();
+
+        String sql = "UPDATE pet SET PetName = ?, Weight = ?, Age = ?, BirthDate = ?, Gender = ?, PetImage = ? WHERE id = ?;";
 
         ExecutorService executionService = Executors.newSingleThreadExecutor();
         executionService.execute(() -> {
             try {
                 PreparedStatement preparedStatement = con.prepareStatement(sql);
                 preparedStatement.setString(1, petName);
-                preparedStatement.setInt(2, weight);
+                preparedStatement.setFloat(2, weight);
                 preparedStatement.setInt(3, age);
                 preparedStatement.setString(4, birthDate);
                 preparedStatement.setString(5, gender);
+                preparedStatement.setBytes(6, imageBytes);
+                preparedStatement.setInt(7, id);
 
                 int rowCount = preparedStatement.executeUpdate();
                 if (rowCount > 0) {
@@ -150,7 +225,7 @@ public class ChangeUserInfoFragment extends Fragment {
         });
     }
 
-    public void connect() {
+    public void connectMysql() {
         ExecutorService executionService = Executors.newSingleThreadExecutor();
         executionService.execute(() -> {
             try {
