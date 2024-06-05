8d291696ddfd6d1c8d11643da392acd2112b082a
Joseph Chiang <joseph900711@gmail.com>
2024-06-05 22:11:55 +0800

    Delete unnecessary package.

--- app/src/main/java/com/example/pet/ui/feed/FeedFragment.java
+++ app/src/main/java/com/example/pet/ui/feed/FeedFragment.java
@@ -3,7 +3,6 @@ package com.example.pet.ui.feed;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
