d04daac2ba6b2000952f1b0a5501bf30737bd270
Joseph Chiang <joseph900711@gmail.com>
2024-06-10 23:01:42 +0800

    Add onSupportNavigateUp() method.

--- app/src/main/java/com/example/pet/MainActivity.java
+++ app/src/main/java/com/example/pet/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -27,14 +28,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_monitor, R.id.navigation_feed, R.id.navigation_healthcare, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
     public void hideBottomNavigationView() {
@@ -45,5 +44,12 @@ public class MainActivity extends AppCompatActivity {
     public void showBottomNavigationView() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
         bottomNavigationView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        return navHostFragment.getNavController().navigateUp() || super.onSupportNavigateUp();
     }
 }
