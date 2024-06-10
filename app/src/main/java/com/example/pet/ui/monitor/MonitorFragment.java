5639bde75a32c879a20dfbf95a0c3b99f9b3a4c9
Joseph Chiang <53220797+joseph0711@users.noreply.github.com>
2024-06-11 00:17:34 +0800

    Update MonitorFragment.java
    Remove the screte string.

--- app/src/main/java/com/example/pet/ui/monitor/MonitorFragment.java
+++ app/src/main/java/com/example/pet/ui/monitor/MonitorFragment.java
@@ -34,7 +34,7 @@ public class MonitorFragment extends Fragment {
         webView.setScrollbarFadingEnabled(false);
         webView.setWebViewClient(new WebViewClient());
 
-        webView.loadUrl("http://20.205.139.170:1880/ui");
+        webView.loadUrl(['Monitor_URL']);
 
         return root;
     }
